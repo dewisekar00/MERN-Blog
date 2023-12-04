c0370e2c7a229112464648b099daf1d8789ccf53
anna <nanamint623@gmail.com>
2023-12-04 09:56:45 +0700

    slicing Login Page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { LoginBg } from '../../assets';
+import { Button, Gap, Input , Link} from '../../components';
+
 
 const Login = () => {
   return (
-    <div>
-      <p>Login Page</p>
+    <div className='main-page'>
+
+    <div className='left'>
+     <img src={LoginBg} className='bg-image' alt='image-register' />
     </div>
+
+    <div className='right'>
+     <p className='title'>Login</p>
+     <Gap height={50} />
+     
+
+     <Input label= "Email " placeholder="email addres"  />
+     <Gap height={16} />
+     <Input label=" password" placeholder="password" t/> 
+
+     <Gap height={20} />
+     <Button title="Login" />
+     <Gap height={50} />
+  
+    <Link  title='Belum punya akun, silahkan daftar'/>
+    </div>
+   </div>
   )
 }
 
