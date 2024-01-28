65559436d46e0ec4cc3eac8c2425de5464e789ee
anna <nanamint623@gmail.com>
2024-01-28 14:54:21 +0700

    implemented add new BlogPost and Upload Image

--- src/components/atoms/Upload/index.js
+++ src/components/atoms/Upload/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import './upload.scss'
-const Upload = () => {
+import React from 'react';
+import './upload.scss';
+const Upload = ({ img, ...rest }) => {
   return (
-    <div className='upload'>
-        < img className='preview' src='https://i.pinimg.com/564x/2e/61/4d/2e614da6668a83dab3078fa534092cec.jpg' alt='dummy-pic'/>
-      < input type='file' />
+    <div className="upload ">
+      {/* kalo image nya ada baru munculin image kalo ngga ada kosongin */}
+      <div className="d-flex justify-content-center">{img && <img className="preview" src={img} alt="image" />}</div>
+      <div className='mx-5'>
+        <input type="file" {...rest} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Upload
+export default Upload;
--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Gap, Input, TextArea, Upload } from '../../components';
 import './createblog.scss';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const CreateBlog = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  // 1.buat state untuk simpan image
+  const [image, setImage] = useState('');
+  const [imagePreview, setImagePreview] = useState(null);
   const navigate = useNavigate();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
+    console.log(body);
+    //4.MENGIRIM 3 DATA(TITLE,BODY,IMAGE KE DATABASE)
+    const data = new FormData();
+    // menambahkan objek baru dengan append
+    data.append('title', title);
+    data.append('body', body);
+    data.append('image', image);
+
+    axios
+      .post('http://localhost:3000/v1/blog/post', data, {
+        headers: {
+          'content-type': 'multipart/form-data',
+        },
+      })
+      .then((res) => {
+        console.log('post success', res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  //2. mengirim foto ke database dan menampilkan previewnya
+
+  const onImageUpload = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+    setImagePreview(URL.createObjectURL(file));
+  };
+
   return (
     <>
-      <Link to="/" onClick={() => navigate('/')}>
-        <span className="material-symbols-outlined">arrow_back_ios</span>
-      </Link>
+      <div className="m-4">
+        <Link to="/" onClick={() => navigate('/')}>
+          <span className="material-symbols-outlined">arrow_back_ios</span>
+        </Link>
+        <form onSubmit={onSubmit}>
+          <div className="createblog-wrap">
+            {/* 3.kasih onChange dan tampikan preview image disini */}
+            <Upload onChange={(e) => onImageUpload(e)} img={imagePreview} />
+          </div>
+          <Gap height={20} />
+          <Input placeholder="Title.." className="input-title" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <TextArea className="text-area" placeholder="Tell your story.." value={body} onChange={(e) => setBody(e.target.value)} />
 
-      <div className="createblog-wrap">
-        <Upload />
-      </div>
-      <Gap height={20} />
-      <Input placeholder="Title.." className="input-title" />
-      <TextArea className="text-area" placeholder="Tell your story.." />
-
-      <div className="button-action">
-        <Button title="save" onClick={() => navigate('/')} />
+          <div className="button-action">
+            <Button title="save" type="submit" />
+          </div>
+        </form>
       </div>
     </>
   );
